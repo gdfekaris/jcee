5bf78c94bd114119b310b081e55ae64fe7b47a2e
George gd Fekaris <gdfekaris@gmail.com>
2019-03-24 17:46:31 -0700

    working on wallet -- verifying signatures

---
+++ wallet/index.js
@@ -0,0 +1,20 @@
+const { STARTING_BALANCE } = require('../config.js');
+const { ec } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
+
+class Wallet {
+  constructor() {
+    this.balance = STARTING_BALANCE;
+
+    this.keyPair = ec.genKeyPair();
+
+    this.publicKey = this.keyPair.getPublic().encode('hex');
+  }
+
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
+  }
+
+}
+
+module.exports = Wallet;
