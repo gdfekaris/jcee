f58db355e7dff00f8d607475052e9311c3b5d41a
George gd Fekaris <gdfekaris@gmail.com>
2019-07-17 17:40:07 -0700

    Added API endpoint for getting wallet address and balance

--- index.js
+++ index.js
@@ -76,6 +76,15 @@ app.get('/api/mine-transactions', (req, res) => {
   res.redirect('/api/blocks');
 });
 
+app.get('/api/wallet-info', (req, res) => {
+  const address = wallet.publicKey;
+
+  res.json({
+    address,
+    balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
+    })
+});
+
 app.post('/api/exchange', (req, res) => {
   const { cards, recipient } = req.body;
 
