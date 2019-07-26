f673cc4dc027594148c9014e69a436686af80084
George gd Fekaris <gdfekaris@gmail.com>
2019-07-25 17:02:14 -0700

    Started front-end development

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>JCEE cryptochain in React! -live update</div>,
+  document.getElementById('root')
+);
+
+console.log('js file is loaded');
