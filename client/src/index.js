c367f362ee1a3d0b7a6f847aea07fede2a4db62f
George gd Fekaris <gdfekaris@gmail.com>
2019-07-31 15:49:40 -0700

    fetch and display wallet info, within App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 render(
-  <div>JCEE cryptochain in React! -live update</div>,
+  <App />,
   document.getElementById('root')
 );
 
